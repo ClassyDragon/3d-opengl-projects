44bacb89a156a037a3a82ac6deedf4d3767fdcbb
ClassyDragon <cgandeg@gmail.com>
2020-01-09 17:53:34 -0500

    3D Crates with Cube class

--- 03_Crates/source.cpp
+++ 03_Crates/source.cpp
@@ -24,8 +24,23 @@
 
 #include "GLCall.h"
 
-void processInput(GLFWwindow* window, glm::vec3& cameraPos, glm::vec3& cameraFront, glm::vec3& cameraUp) {
-    const float cameraSpeed = 0.05f;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+float lastX = 400.0f;
+float lastY = 300.0f;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+bool firstMouse = true;
+
+glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
+
+void processInput(GLFWwindow* window) {
+    float cameraSpeed = 2.5f * deltaTime;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         cameraPos += cameraSpeed * cameraFront;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -36,6 +51,37 @@ void processInput(GLFWwindow* window, glm::vec3& cameraPos, glm::vec3& cameraFro
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates rance from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    const float sensitivity = 0.05f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw   += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+
 int main() {
     GLFWwindow* window;
 
@@ -43,9 +89,11 @@ int main() {
         return -1;
     }
 
-    window = glfwCreateWindow(700, 700, "Tetromino", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Tetromino", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouseCallback);
 
     glewInit();
 
@@ -58,15 +106,15 @@ int main() {
 
     glm::mat4 proj, view, model;
     glm::mat4 trans(1.0f);
-    glm::mat4 perspectiveProjection = glm::perspective(glm::radians(40.0f), 500.0f / 500.0f, 0.1f, 100.f);
+    glm::mat4 perspectiveProjection = glm::perspective(glm::radians(40.0f), 800.0f / 600.0f, 0.1f, 100.f);
     proj = perspectiveProjection;
     view = glm::lookAt(glm::vec3(0, 4, 10), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
     model = glm::mat4(1.0f);
 
     // Camera
-    glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
-    glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
-    glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
+//    glm::vec3 cameraPos(0.0f, 0.0f, 3.0f);
+//    glm::vec3 cameraFront(0.0f, 0.0f, -1.0f);
+//    glm::vec3 cameraUp(0.0f, 1.0f, 0.0f);
 
     float degrees[] = {
         0.0f, 0.0f, 0.0f
@@ -79,10 +127,10 @@ int main() {
     BatchCubes bc(&cube);
     bc.insertCube(glm::mat4(1.0));
     glm::mat4 trans1(1.0);
-    trans1 = glm::translate(trans1, glm::vec3(2.0, 0.0, 0.0));
+    trans1 = glm::translate(trans1, glm::vec3(2.2, 0.0, 0.0));
     bc.insertCube(trans1);
     glm::mat4 trans2(1.0);
-    trans2 = glm::translate(trans2, glm::vec3(1.0, 2.0, 0.0));
+    trans2 = glm::translate(trans2, glm::vec3(1.0, 2.1, 0.0));
     bc.insertCube(trans2);
 
     Shader shader("res/shaders/shader1.glsl");
@@ -95,6 +143,10 @@ int main() {
     glDepthFunc(GL_LESS);
 
     while (!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         trans = glm::mat4(1.0f);
         trans = glm::translate(trans, glm::vec3(units[0], 0.0f, 0.0f));
@@ -113,7 +165,7 @@ int main() {
         renderer.Clear();
         glClear(GL_DEPTH_BUFFER_BIT);
         renderer.Draw(bc, shader);
-        processInput(window, cameraPos, cameraFront, cameraUp);
+        processInput(window);
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
